b32b87359620b4798b21f7bd77b4d57c0bbb8460
Cheeettoozzz <Cheeettoozzz@DESKTOP-6P23GR7>
2018-09-04 16:57:24 +0700

    Studi Kasus

---
+++ src/kalkulator/txt_hasil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulator;
+
+/**
+ *
+ * @author Cheeettoozzz
+ */
+class txt_hasil {
+
+    static void setText(String bilangan) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Object getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setText(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
